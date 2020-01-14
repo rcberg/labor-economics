1abeff8282e91a8ba7cc15f9deb540b9cf180f28
Chris Berg <cberg@uoregon.edu>
2020-01-14 12:53:53 -0800

    decided to name the parameters more clearly :^)

--- discrimination_simulation.R
+++ discrimination_simulation.R
@@ -1,19 +1,19 @@
-a_O = 0
-a_W = 10
-sd_O = 1
-b_d = 1 # parameter governing discrimination penalty "women" face (0-1)
-b_O_disc = 1
-b_O_ability = 2
-b_W_occ = 2
-b_W_disc = 1
-b_W_ability = 2
+a_occ = 0
+a_w = 10
+sd_w = 1
+b_disc = 1 # parameter governing discrimination penalty "women" face (0-1)
+b_occ_disc = 1
+b_occ_ability = 2
+b_w_occ = 2
+b_w_disc = 1
+b_w_ability = 2
 
 women = rbinom(10000 , 1 , 0.5)
 ability = rnorm(10000)
 
-discrimination = b_d*women
-occupation = a_O - b_O_disc*discrimination + b_O_ability*ability + rnorm(10000, mean = 0 , sd = sd_O)
-wage = a_W + b_W_occ*occupation + b_W_ability*ability + rnorm(10000)
+discrimination = b_disc*women
+occupation = a_occ - b_occ_disc*discrimination + b_occ_ability*ability + rnorm(10000, mean = 0 , sd = sd_O)
+wage = a_W + b_w_occ*occupation + b_w_ability*ability + rnorm(10000)
 
 true_wage = lm(wage ~ occupation + ability)
 ecms_wage = lm(wage ~ women )
