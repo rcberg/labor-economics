3900f0820d85308c7527085fa539eee46e6f0a50
Chris Berg <cberg@uoregon.edu>
2020-01-14 12:43:44 -0800

    "better" simulation for discrimination

--- discrimination_simulation.R
+++ discrimination_simulation.R
@@ -1,4 +1,4 @@
-a_O = 100
+a_O = 0
 a_W = 10
 sd_O = 0.5
 b_d = 1 # parameter governing discrimination penalty "women" face (0-1)
@@ -12,11 +12,12 @@ women = rbinom(10000 , 1 , 0.5)
 ability = rnorm(10000)
 
 discrimination = b_d*women
-occupation = a_O - b_O_disc*discrimination + b_O_ability * ability + rnorm(10000, mean = 0 , sd = sd_O)
-wage = a_W + b_W_occ*occupation - b_W_disc*discrimination + b_W_ability*ability + rnorm(10000)
+occupation = a_O - b_O_disc*discrimination + b_O_ability*ability + rnorm(10000, mean = 0 , sd = sd_O)
+wage = a_W + b_W_occ*occupation + b_W_ability*ability + rnorm(10000)
 
 true_wage = lm(wage ~ occupation + discrimination + ability)
-ecms_wage = lm(wage ~ occupation + women )
+ecms_wage = lm(wage ~ women )
+ecms_wage_bad = lm(wage ~ occupation + women)
 
-summary(true_wage)
 summary(ecms_wage)
+summary(ecms_wage_bad) # estimates positive effects!! wtf? simulations rool
